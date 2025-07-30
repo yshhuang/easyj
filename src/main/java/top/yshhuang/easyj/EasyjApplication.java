a181b1fb8dabe9fa641b2b64f5e8f67294b0c84f
yshhuang <yshhuang@foxmail.com>
2025-07-30 22:36:14 +0800

    first commit

---
+++ src/main/java/top/yshhuang/easyj/EasyjApplication.java
@@ -0,0 +1,13 @@
+package top.yshhuang.easyj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/top/yshhuang/easyj/EasyjApplicationTests.java
@@ -0,0 +1,13 @@
+package top.yshhuang.easyj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
